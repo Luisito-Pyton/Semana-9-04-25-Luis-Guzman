a5a3afd1c57c530464be8fa4b87e8e11013b0b2a
Luisito-Pyton <luisito222da@gmail.com>
2025-04-09 09:08:44 -0500

    ejercicio1.py

--- Taller de encapsulamiento/ejercicio1.py
+++ Taller de encapsulamiento/ejercicio1.py
@@ -6,12 +6,10 @@ class Estudiante:
     def mostrar_datos(self):
         return f'Nombre: {self.__nombre}, Notas: {self.__notas}'
 
-# Crear 3 objetos Estudiante
 estudiante1 = Estudiante('Juan', [85, 90, 78])
 estudiante2 = Estudiante('Ana', [92, 88, 95])
 estudiante3 = Estudiante('Luis', [75, 80, 70])
 
-# Mostrar datos
 print(estudiante1.mostrar_datos())
 print(estudiante2.mostrar_datos())
 print(estudiante3.mostrar_datos())
